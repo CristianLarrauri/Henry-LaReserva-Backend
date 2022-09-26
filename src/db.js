fa7cb8f5467e796f1ec3571fe0eaa20d100ff5f5
Matias Franco <matias.franco289@gmail.com>
2022-09-26 03:02:08 -0300

    'AAAAAAAAAA'

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, PORT, DB_NAME } = process.env;
 
 console.log("Entrando al app");
 
@@ -10,9 +10,10 @@ const sequelize = new Sequelize(
 	`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/reserva`,
 	{
 		logging: false, // set to console.log to see the raw SQL queries
-		native: false // lets Sequelize know we can use pg-native for ~30% more speed
+		native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 	}
 );
+
 
 console.log('sigo vivo');
 const basename = path.basename(__filename);

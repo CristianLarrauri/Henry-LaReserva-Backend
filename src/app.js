aaf51aecb6af99393a10b9af89e89468486997a5
Matias Franco <matias.franco289@gmail.com>
2022-09-26 02:35:14 -0300

    asdasd

--- src/app.js
+++ src/app.js
@@ -9,13 +9,18 @@ require("./db.js");
 
 const server = express();
 
-console.log("Entrando al app");
+console.log("Entrando al app 1");
 server.name = "API";
 
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+console.log("Entrando al app 2");
 server.use(bodyParser.json({ limit: "50mb" }));
+console.log("Entrando al app 3");
 server.use(cookieParser());
+console.log("Entrando al app 4");
 server.use(morgan("dev"));
+console.log("Entrando al app 5");
+
 server.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", HOST); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
@@ -28,9 +33,11 @@ server.use((req, res, next) => {
 });
 
 server.use("/", routes);
+console.log("Entrando al app 6");
 
 // Error catching endware.
 server.use((err, req, res, next) => {
+  console.log("Entrando al app 7");
   // eslint-disable-line no-unused-vars
   const status = err.status || 500;
   const message = err.message || err;

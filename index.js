9ad05e4862057f5527cf9aac3d80d3d5012f0010
Matias Franco <matias.franco289@gmail.com>
2022-09-26 04:35:37 -0300

    asdasd

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const server = require("./src/app.js");
 const express = require("express");
 const { conn } = require("./src/db.js");
 const morgan = require("morgan");
-const { DB_PORT } = process.env;
+const { PORT } = process.env;
 
 const { preload_players } = require("./src/utils/utilsPlayers.js");
 const { preload_teams } = require("./src/utils/utilsTeams.js");
@@ -15,7 +15,7 @@ app.use(morgan("dev"));
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(DB_PORT, () => {
+  server.listen(PORT, () => {
     console.log("entrando al index pero bien adentro culia");
     preload_tournaments();
     preload_players();

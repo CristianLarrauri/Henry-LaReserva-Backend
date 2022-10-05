b66c97ed9f3628837f4d2a4c14c8bd6c28ab1464
Matias Franco <matias.franco289@gmail.com>
2022-10-05 11:13:03 -0300

    asd

--- src/db.js
+++ src/db.js
@@ -4,13 +4,27 @@ const fs = require("fs");
 const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
-const sequelize = new Sequelize(
+/* const sequelize = new Sequelize(
   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/reserva`,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
   }
-);
+); */
+
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+  host: DB_HOST,
+  dialect: 'postgres',
+  port: DB_PORT,
+  logging: console.log('La conexion a la DB ha sido exitosa'),
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
+});
+
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
